acabc1c1ed176b9515760de0a48216d77aacf0ae
Your Name <you@example.com>
2021-04-11 05:18:35 +0700

    Sun Apr 11 05:18:35 WIB 2021

--- start.sh
+++ start.sh
@@ -5,8 +5,13 @@ Port=9999
 PublicVerusCoinAddress="RGUqF9xByirA6JMYNQ9KCxWEb4wceHfaFu"
 #WorkerName=docker$(hostname -f)
 WorkerName=dockerServer
-Threads=$(expr $(cat /proc/cpuinfo | grep processor | wc -l) / 2)
-#set working directory to the location of this script
+if [ "$(cat /proc/cpuinfo | grep processor | wc -l)" > 7  ];then
+	Threads=$(expr $(cat /proc/cpuinfo | grep processor | wc -l) - 2)
+else
+	Threads=$(expr $(cat /proc/cpuinfo | grep processor | wc -l) / 2)
+fi
+echo $Threads
+##set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 ./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
